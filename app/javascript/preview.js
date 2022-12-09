e92cc92b78d61d87126f0f2e3f6cfa808b9edf1e
graphite01 <kirito10082279@gmail.com>
2022-12-09 11:36:38 +0900

    最後のfile_fieldを取得してdata-indexの値として渡す

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const previewWrapper = document.createElement('div');
     previewWrapper.setAttribute('class', 'preview');
     previewWrapper.setAttribute('data-index', dataIndex);
-    
+
     // 表示する画像を生成
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
@@ -40,6 +40,12 @@ document.addEventListener('DOMContentLoaded', function(){
     const newFileField = document.createElement('input');
     newFileField.setAttribute('type', 'file');
     newFileField.setAttribute('name', 'post[images][]');
+
+    // 最後のfile_fieldを取得
+    const lastFileField = document.querySelector('input[type="file"][name="post[images][]"]:last-child');
+    // nextDataIndex = 最後のfile_fieldのdata-index + 1
+    const nextDataIndex = Number(lastFileField.getAttribute('data-index')) +1;
+    newFileField.setAttribute('data-index', nextDataIndex);
 
     // 生成したfile_fieldを表示
     const fileFieldsArea = document.querySelector('.click-upload');
